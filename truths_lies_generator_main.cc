f79482c2d969e4328b142e5f6f999f8420ebb130
Travis Chen <travis.chen@everchanging.dev>
2020-11-01 17:28:13 -0800

    truths_lies_generator_main.cc: ensure no duplicates

--- truths_lies_generator_main.cc
+++ truths_lies_generator_main.cc
@@ -24,6 +24,8 @@ ABSL_FLAG(bool, random_order, false,
           "if not set, output is sorted lexically");
 ABSL_FLAG(int, lies, 0, "number of lie statements");
 ABSL_FLAG(int, truths, 0, "number of truth statements");
+ABSL_FLAG(int, max_retries, 10,
+          "number of times to retry if we generate identical statements");
 ABSL_FLAG(std::vector<std::string>, input_files, {}, "input data file");
 
 namespace {
@@ -35,6 +37,13 @@ typedef struct Statement {
   bool truth;
 } Statement;
 
+struct SharedStatementCmp {
+  bool operator()(const std::shared_ptr<Statement>& lhs,
+                  const std::shared_ptr<Statement>& rhs) const {
+    return lhs->statement < rhs->statement;
+  }
+};
+
 int main(int argc, char** argv) {
   absl::ParseCommandLine(argc, argv);
 
@@ -43,6 +52,7 @@ int main(int argc, char** argv) {
   std::vector<std::set<std::string>> allTruthsPerGenerator;
 
   std::vector<std::shared_ptr<Statement>> statementsVector;
+  std::set<std::shared_ptr<Statement>, SharedStatementCmp> statementsSet;
 
   for (std::string& input_file : absl::GetFlag(FLAGS_input_files)) {
     TruthsLiesConfig config;
@@ -77,32 +87,48 @@ int main(int argc, char** argv) {
   std::uniform_real_distribution<> ud(0.0, 1.0);
 
   for (int i = 0; i < absl::GetFlag(FLAGS_truths); ++i) {
-    int generatorIndex = dd(gen);
-    // generatorWeights represent the size of each valueMap vector
-    int valueMapIndex = static_cast<int>(
-        generatorWeights[generatorIndex] * ud(gen));
     std::shared_ptr<Statement> s = std::make_shared<Statement>(Statement {
+      .truth = true,
+    });
+    int retries_left = absl::GetFlag(FLAGS_max_retries);
+    do {
+      int generatorIndex = dd(gen);
+      // generatorWeights represent the size of each valueMap vector
+      int valueMapIndex = static_cast<int>(
+          generatorWeights[generatorIndex] * ud(gen));
       // if ensure not true, we already have all the statements; but because we
       // have them as a set, access will be O(n); as such it's a bit more
       // efficient to just compute the truth statement again
-      .statement = statementGenerators[generatorIndex]->truth(valueMapIndex),
-      .truth = true,
-    });
+      s->statement = statementGenerators[generatorIndex]->truth(valueMapIndex);
+    } while (statementsSet.count(s) > 0 && retries_left-- > 0);
+    if (retries_left <= 0) {
+      std::cerr << "ERROR: generated too many duplicate statements" << std::endl;
+      return EXIT_FAILURE;
+    }
+    statementsSet.insert(s);
     statementsVector.push_back(s);
   }
 
   for (int i = 0; i < absl::GetFlag(FLAGS_lies); ++i) {
-    int generatorIndex = dd(gen);
-    // generatorWeights represent the size of each valueMap vector
-    int valueMapIndex = static_cast<int>(
-        generatorWeights[generatorIndex] * ud(gen));
-    std::string lie;
+    int generatorIndex;
     std::shared_ptr<Statement> s = std::make_shared<Statement>(Statement {
       .truth = false,
     });
+    int retries_left = absl::GetFlag(FLAGS_max_retries);
     do {
+      generatorIndex = dd(gen);
+      // generatorWeights represent the size of each valueMap vector
+      int valueMapIndex = static_cast<int>(
+          generatorWeights[generatorIndex] * ud(gen));
       s->statement = statementGenerators[generatorIndex]->lie(valueMapIndex);
-    } while (allTruthsPerGenerator[generatorIndex].count(s->statement) > 0);
+    } while (
+        allTruthsPerGenerator[generatorIndex].count(s->statement) > 0 &&
+        statementsSet.count(s) > 0 && retries_left-- > 0);
+    if (retries_left <= 0) {
+      std::cerr << "ERROR: generated too many duplicate statements" << std::endl;
+      return EXIT_FAILURE;
+    }
+    statementsSet.insert(s);
     statementsVector.push_back(s);
   }
 
